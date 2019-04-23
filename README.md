4efbb35d5676730aef2ddf8b6f1cf72b10a9b213
brentjm <brent_maranzano@yahoo.com>
2019-04-23 13:43:46 -0400

    Intial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# PostgreSQL
+Setting up a PostgreSQL server in Docker container
+
+## Getting started
+1. clone the repository
+2. create docker volume
+* docker volume create postgres
+3. run the docker script
+
+# Author
+
+**Brent Maranzano**
+
+# License
+
+This project is licensed under the MIT License - see the LICENSE file for details
---
+++ docker_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+################################################################################
+# Script name: docker_run.sh
+# Description: Runs a container with PostgreSQL
+# Args: None
+# Author: Brent Maranzno
+# email: brent_maranzano@gmail.com
+
+# Usage:
+# create a Docker volume: postgres
+# see: https://hub.docker.com/_/postgres
+################################################################################
+docker run --detach=true --name postgres-custom \
+    --env POSTGRES_PASSWORD=postgres24.27 \
+    --publish 5432:5432/tcp \
+    --mount 'type=volume,source=postgres,target=/var/lib/postgresql/data' \
+    postgres
